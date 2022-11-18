55fcbf2dba21864db96bfccecb90cdc63383da33
Miller <millerfsalcedob@gmail.com>
2022-11-18 11:24:49 -0500

    curso terminado

---
+++ components/dateElement.js
@@ -0,0 +1,8 @@
+export default (date) =>{
+
+    const dateElement = document.createElement("li");
+    dateElement.classList.add("date");
+    dateElement.innerHTML = date;
+    return dateElement;
+
+}
